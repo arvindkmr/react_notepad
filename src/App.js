00b354d8e6c7e76bcd499a68933220651cd6a417
arvind <developer.arvind003@gmail.com>
2023-02-04 09:26:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [name,setName] =useState('');
+  const [age,setAge] =useState();
+  const [task,setTask] =useState('');
+  const [data,setData] =useState([{name:"abc",age:21,task:"testing"}]);
+  
+  const saveData = ()=>{
+    setData(...data, {name:'name',age:age,task:'task'})
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="input1">
+          <label htmlFor="Name">Name</label>
+          <input type="text" className="input" value={name} onChange={(event)=> setName(event.target.value)}/>
+        </div>
+        <div className="input2">
+          <label htmlFor="Age">Age</label>
+          <input type="number" className="input" value={age} onChange={(event)=> setAge(event.target.value)}/>
+        </div>
+        <div className="input3">
+          <label htmlFor="Age">Task </label>
+          <input type="text" className="input" value={task} onChange={(event)=> setTask(event.target.value)}/>
+        </div>
+        <div className="savelist">
+          <button className="save" onClick={saveData}>Save data</button>
+        </div>
+        <div className="display">
+          <ul>
+          {data.map((item )=>{
+            {/* const {name}=item */}
+            console.log(item)
+            return <li>
+              {item}
+              abc
+            </li>
+          })}
+          </ul>
+        </div>
       </header>
     </div>
   );
